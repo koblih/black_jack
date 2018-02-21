0599185c471828272ccae892ff225d857ee6ceb7
Lenka Klement <lenka.koblih@gmail.com>
2018-02-21 22:19:52 +0000

    functioning value plus preliminary loop

--- black_jack.py
+++ black_jack.py
@@ -14,10 +14,7 @@ class Card(object):
                      
         return str(self.card)
 
-    def card_value(self):
-        return self.card[0]
-
-# __getitem__ to get the value
+# to be able to get the value of the card
     def __getitem__(self, key):
         return self.card[key]
 
@@ -26,7 +23,9 @@ class Deck(object):
     def __init__(self):
         self.deck = []
         self.create_deck()
-       
+
+# create deck using hard coded values of cards
+
     def create_deck(self):       
         for suit in suits:
             for rank in ranks:
@@ -45,28 +44,44 @@ class Hand(Deck):
         self.hand = [] 
         self.hand_value = 0
 
-    def get_card(self):
-        selection = random.choice(self.deck)
+# selecting a random card from the deck
+# TO BE DONE - make sure appended card has always index 0 
+
+#    def get_card(self):
+#        selection = random.choice(self.deck)
+#        self.hand.append(selection)
+#        print 'Your hand: {}'.format(self.hand)
+
+# testing of hand value loop
+
+    def test(self):
+        selection = Card(2, 'Hearts')
         self.hand.append(selection)
-        print 'Your hand: {}'.format(self.hand)
+        print '{}'.format(self.hand)
 
 # get value of the randomly selected card and add it to the hand value
 
     def get_value(self):
-        print self.hand
-        print self.hand[0][1]
+        card_value = self.hand[0][0]
+        if card_value in range(1,10):
+            self.hand_value += card_value
+        elif (card_value == 'J' or card_value == 'Q' or card_value == 'K'):
+            self.hand_value += 10
+        else:
+            self.hand_value += 1
+        print '{}'.format(self.hand_value)
 
 def main():
     
-    card = Card('ace', 'hearts')
-    print card
-    print card.card_value()
+#    card = Card('ace', 'hearts')
+#    print card
 
 #    deck = Deck()
 #    print deck
 
     hand = Hand()
-    hand.get_card()
+#    hand.get_card()
+    hand.test()
     hand.get_value()
 
         
