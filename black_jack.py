d1294631098bf09dd219c34c18254e39346d91cc
Lenka Klement <lenka.koblih@gmail.com>
2018-03-11 19:39:12 +0000

    main() beginning

--- black_jack.py
+++ black_jack.py
@@ -3,6 +3,8 @@ import random
 suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
 ranks = ['A',1,2,3,4,5,6,7,8,9,10,'J','Q','K']
 
+# card as a list of two values
+
 class Card(object):
 
     def __init__(self, rank, suit):
@@ -11,12 +13,13 @@ class Card(object):
         self.card = [self.rank, self.suit]  
 
     def __repr__(self):
-                     
         return str(self.card)
 
-# to be able to get the value of the card
+# to be able to get the value of the card - make iterable
     def __getitem__(self, key):
         return self.card[key]
+
+# deck as list of lists
 
 class Deck(object):
        
@@ -25,11 +28,10 @@ class Deck(object):
         self.create_deck()
 
 # create deck using hard coded values of cards
-
     def create_deck(self):       
         for suit in suits:
             for rank in ranks:
-                self.deck.append(Card(rank, suit))
+                self.deck.append(Card(rank, suit))  
         
     def __repr__(self):
         return 'The current deck contains: {}'.format(self.deck)
@@ -45,21 +47,14 @@ class Hand(Deck):
         self.hand_value = 0
 
 # selecting a random card from the deck 
-
     def get_card(self):
         selection = random.choice(self.deck)
         self.hand.insert(0,selection)
+        self.deck.remove(selection)
         print 'Your hand: {}'.format(self.hand)
 
-# testing of hand value loop
-
-#    def test_card_value(self):
-#        selection = Card('A', 'Hearts')
-#        self.hand.append(selection)
-#        print '{}'.format(self.hand)
-
 # get value of the randomly selected card and add it to the hand value
-
+# this means getting the first [0] item of the card list selected randomly from the deck list and appended to the hand list
     def get_value(self):
         card_value = self.hand[0][0]
         if card_value in range(1,10):
@@ -72,16 +67,29 @@ class Hand(Deck):
             else:
                 self.hand_value += 1
         print '{}'.format(self.hand_value)
+  
+
+# create two hands for each player (for now one pleayer against computer)
+# maybe ask how many players will play?
+# ask the players to put the names in
+# after the names are in deal two cards to each player
+# ask one by one if they want another card to be dealt (note this way the other players will always know the hand value of other players)
+# display the hand and the hand value
+# depending on hand value ask if they want more, or tell them they are bust
+# calculate if there is a winner and display the relevant message
 
 def main():
-    
-    hand = Hand()
-    hand.get_card()
-#    hand.test_card_value()
-    hand.get_value()
-    hand.get_card()
-    hand.get_value()
-    hand.get_card()
-    hand.get_value()
-    
-main()
+
+    deck = Deck()
+
+    while True:
+        no_of_players = raw_input('Please enter the number of players: ')
+        if not no_of_players.isdigit():
+            print 'Please enter the NUMBER of players! '
+            continue
+        else:
+            pass
+
+
+
+main()    
